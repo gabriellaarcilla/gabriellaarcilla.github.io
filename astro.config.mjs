b63da7201b72795c6252021e63560be9eb4b1b30
Gabriell Arcilla <arcillagaby@gmail.com>
2026-06-29 16:38:47 -0700

    updating author settings

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://whydevils.github.io',
-  base: '/astro-scholar/',
+  site: 'https://gabriellaarcilla.github.io/',
+  base: '/some-name/',
 });
